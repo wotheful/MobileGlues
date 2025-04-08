670ca4514a683067e8c2431ff0d139d2c7d97249
Swung0x48 <swung0x48@outlook.com>
2025-04-08 17:04:20 +0800

    [misc] (state): more states to hijack

--- src/main/cpp/gl/fpe/state.cpp
+++ src/main/cpp/gl/fpe/state.cpp
@@ -33,6 +33,7 @@ bool hijack_fpe_states(GLenum cap, bool enable, fixed_function_bool_t* bools) {
         case GL_LIGHT6:
         case GL_LIGHT7:
         case GL_TEXTURE_2D:
+        case GL_RESCALE_NORMAL:
             return true;
         default:
             break;
