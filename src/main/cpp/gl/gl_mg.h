c77ca379a4fa17c8600c6d0e6fca780c64610826
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-04-13 00:06:44 +0800

    Update gl_mg.h

--- src/main/cpp/gl/gl_mg.h
+++ src/main/cpp/gl/gl_mg.h
@@ -13,6 +13,11 @@ extern "C" {
 
 GLAPI GLAPIENTRY void glClearDepth( GLclampd depth );
 GLAPI GLAPIENTRY void glHint( GLenum target, GLenum mode );
+GLAPI GLAPIENTRY void glDepthMask (GLboolean flag);
+GLAPI GLAPIENTRY void glColorMask (GLboolean red, GLboolean green, GLboolean blue, GLboolean alpha);
+GLAPI GLAPIENTRY void glViewport (GLint x, GLint y, GLsizei width, GLsizei height);
+GLAPI GLAPIENTRY void glClear (GLbitfield mask);
+GLAPI GLAPIENTRY void glClearColor (GLfloat red, GLfloat green, GLfloat blue, GLfloat alpha);
 
 #ifdef __cplusplus
 }
