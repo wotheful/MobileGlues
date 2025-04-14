7022db9925b813207db9dab1a6216fe5e7a76e05
Swung0x48 <swung0x48@outlook.com>
2025-04-14 23:28:10 +0800

    [Feat] (test): add sanity test?

---
+++ src/main/cpp/tests/sanitycheck_test.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Swung 0x48 on 2025/4/14.
+//
+#include "gtest/gtest.h"
+
+TEST(sanity, sanity) { EXPECT_EQ(1 + 1, 3); }
---
+++ src/test/java/com/fcl/plugin/mobileglues/SanityCheckTest.kt
@@ -0,0 +1,9 @@
+package com.fcl.plugin.mobileglues
+
+import androidx.test.ext.junitgtest.GtestRunner
+import androidx.test.ext.junitgtest.TargetLibrary
+import org.junit.runner.RunWith
+
+@RunWith(GtestRunner::class)
+@TargetLibrary(libraryName = "mobileglues_tests")
+class SanityCheckTest
