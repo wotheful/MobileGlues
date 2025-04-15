f57604c6603eb6543d3a6998810265ef179f1403
Swung0x48 <swung0x48@outlook.com>
2025-04-15 17:41:43 +0800

    [Test] (vertexpointer_test): VertexPointer, PointerNormalizeMix

--- src/main/cpp/tests/fpe/vertexpointer_test.cpp
+++ src/main/cpp/tests/fpe/vertexpointer_test.cpp
@@ -242,3 +242,170 @@ TEST(VertexPointer, PointerNormalize0x85) {
         EXPECT_EQ(tex_va.pointer, (const void*) 0xc);
     }
 }
+
+TEST(VertexPointer, PointerNormalizeMix) {
+    auto& vpa = g_glstate.fpe_state.vertexpointer_array;
+
+    vpa.reset();
+
+    glVertexPointer(3, GL_FLOAT, 24, (const void*)0xe9d6a000);
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glTexCoordPointer(2, GL_FLOAT, 24, (const void*)0xe9d6a00c);
+    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+    glColorPointer(4, GL_UNSIGNED_BYTE, 24, (const void*)0xe9d6a014);
+    glEnableClientState(GL_COLOR_ARRAY);
+
+    vpa.normalize();
+
+    EXPECT_EQ((uint64_t) 0xe9d6a000,
+              (uint64_t) vpa.starting_pointer);
+
+    EXPECT_EQ((uint64_t) 0x85,
+              (uint64_t) vpa.enabled_pointers);
+
+    EXPECT_EQ((uint64_t) 24,
+              (uint64_t) vpa.stride);
+
+    {
+        auto &vertex_va = vpa.attributes[vp2idx(GL_VERTEX_ARRAY)];
+        EXPECT_EQ(vertex_va.size, 3);
+        EXPECT_EQ(vertex_va.usage, GL_VERTEX_ARRAY);
+        EXPECT_EQ(vertex_va.type, GL_FLOAT);
+        EXPECT_EQ(vertex_va.stride, 24);
+        EXPECT_EQ(vertex_va.pointer, (const void *) 0);
+
+        auto &color_va = vpa.attributes[vp2idx(GL_COLOR_ARRAY)];
+        EXPECT_EQ(color_va.size, 4);
+        EXPECT_EQ(color_va.usage, GL_COLOR_ARRAY);
+        EXPECT_EQ(color_va.type, GL_UNSIGNED_BYTE);
+        EXPECT_EQ(color_va.stride, 24);
+        EXPECT_EQ(color_va.pointer, (const void *) 0x14);
+
+        auto &tex_va = vpa.attributes[vp2idx(GL_TEXTURE_COORD_ARRAY)];
+        EXPECT_EQ(tex_va.size, 2);
+        EXPECT_EQ(tex_va.usage, GL_TEXTURE_COORD_ARRAY);
+        EXPECT_EQ(tex_va.type, GL_FLOAT);
+        EXPECT_EQ(tex_va.stride, 24);
+        EXPECT_EQ(tex_va.pointer, (const void *) 0xc);
+    }
+    // Check for idempotence
+    for (int i = 0; i < 5; ++i) {
+        vpa.normalize();
+
+        EXPECT_EQ((uint64_t) 0xe9d6a000,
+                  (uint64_t) vpa.starting_pointer);
+
+        EXPECT_EQ((uint64_t) 0x85,
+                  (uint64_t) vpa.enabled_pointers);
+
+        EXPECT_EQ((uint64_t) 24,
+                  (uint64_t) vpa.stride);
+
+        auto& vertex_va = vpa.attributes[vp2idx(GL_VERTEX_ARRAY)];
+        EXPECT_EQ(vertex_va.size, 3);
+        EXPECT_EQ(vertex_va.usage, GL_VERTEX_ARRAY);
+        EXPECT_EQ(vertex_va.type, GL_FLOAT);
+        EXPECT_EQ(vertex_va.stride, 24);
+        EXPECT_EQ(vertex_va.pointer, (const void*)0);
+
+        auto& color_va = vpa.attributes[vp2idx(GL_COLOR_ARRAY)];
+        EXPECT_EQ(color_va.size, 4);
+        EXPECT_EQ(color_va.usage, GL_COLOR_ARRAY);
+        EXPECT_EQ(color_va.type, GL_UNSIGNED_BYTE);
+        EXPECT_EQ(color_va.stride, 24);
+        EXPECT_EQ(color_va.pointer, (const void*) 0x14);
+
+        auto& tex_va = vpa.attributes[vp2idx(GL_TEXTURE_COORD_ARRAY)];
+        EXPECT_EQ(tex_va.size, 2);
+        EXPECT_EQ(tex_va.usage, GL_TEXTURE_COORD_ARRAY);
+        EXPECT_EQ(tex_va.type, GL_FLOAT);
+        EXPECT_EQ(tex_va.stride, 24);
+        EXPECT_EQ(tex_va.pointer, (const void*) 0xc);
+    }
+
+    // OpenGL code should take care of this using glEnable/DisableClientState
+    vpa.enabled_pointers = 0;
+
+    struct test_struct_t {
+        float pos[4];
+        float normal[3];
+        float uv[2];
+    } ts;
+
+    glVertexPointer(4, GL_FLOAT, sizeof(test_struct_t), ts.pos);
+    glNormalPointer(GL_FLOAT, sizeof(test_struct_t), ts.normal);
+    glTexCoordPointer(2, GL_FLOAT, sizeof(test_struct_t), ts.uv);
+
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_NORMAL_ARRAY);
+    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
+    vpa.normalize();
+
+    EXPECT_EQ((uint64_t) &ts,
+              (uint64_t) vpa.starting_pointer);
+
+    EXPECT_EQ((uint64_t) 0x83,
+              (uint64_t) vpa.enabled_pointers);
+
+    EXPECT_EQ((uint64_t) sizeof(test_struct_t),
+              (uint64_t) vpa.stride);
+
+    {
+        auto &vertex_va = vpa.attributes[vp2idx(GL_VERTEX_ARRAY)];
+        EXPECT_EQ(vertex_va.size, 4);
+        EXPECT_EQ(vertex_va.usage, GL_VERTEX_ARRAY);
+        EXPECT_EQ(vertex_va.type, GL_FLOAT);
+        EXPECT_EQ(vertex_va.stride, sizeof(test_struct_t));
+        EXPECT_EQ(vertex_va.pointer, (const void *) 0);
+
+        auto &normal_va = vpa.attributes[vp2idx(GL_NORMAL_ARRAY)];
+        EXPECT_EQ(normal_va.size, 3);
+        EXPECT_EQ(normal_va.usage, GL_NORMAL_ARRAY);
+        EXPECT_EQ(normal_va.type, GL_FLOAT);
+        EXPECT_EQ(normal_va.stride, sizeof(test_struct_t));
+        EXPECT_EQ(normal_va.pointer, (const void *) offsetof(test_struct_t, normal));
+
+        auto &tex_va = vpa.attributes[vp2idx(GL_TEXTURE_COORD_ARRAY)];
+        EXPECT_EQ(tex_va.size, 2);
+        EXPECT_EQ(tex_va.usage, GL_TEXTURE_COORD_ARRAY);
+        EXPECT_EQ(tex_va.type, GL_FLOAT);
+        EXPECT_EQ(tex_va.stride, sizeof(test_struct_t));
+        EXPECT_EQ(tex_va.pointer, (const void *) offsetof(test_struct_t, uv));
+    }
+
+    // Check for idempotence
+    for (int i = 0; i < 5; ++i) {
+        vpa.normalize();
+
+        EXPECT_EQ((uint64_t) &ts,
+                  (uint64_t) vpa.starting_pointer);
+
+        EXPECT_EQ((uint64_t) 0x83,
+                  (uint64_t) vpa.enabled_pointers);
+
+        EXPECT_EQ((uint64_t) sizeof(test_struct_t),
+                  (uint64_t) vpa.stride);
+
+        auto& vertex_va = vpa.attributes[vp2idx(GL_VERTEX_ARRAY)];
+        EXPECT_EQ(vertex_va.size, 4);
+        EXPECT_EQ(vertex_va.usage, GL_VERTEX_ARRAY);
+        EXPECT_EQ(vertex_va.type, GL_FLOAT);
+        EXPECT_EQ(vertex_va.stride, sizeof(test_struct_t));
+        EXPECT_EQ(vertex_va.pointer, (const void*)0);
+
+        auto& normal_va = vpa.attributes[vp2idx(GL_NORMAL_ARRAY)];
+        EXPECT_EQ(normal_va.size, 3);
+        EXPECT_EQ(normal_va.usage, GL_NORMAL_ARRAY);
+        EXPECT_EQ(normal_va.type, GL_FLOAT);
+        EXPECT_EQ(normal_va.stride, sizeof(test_struct_t));
+        EXPECT_EQ(normal_va.pointer, (const void*) offsetof(test_struct_t, normal));
+
+        auto& tex_va = vpa.attributes[vp2idx(GL_TEXTURE_COORD_ARRAY)];
+        EXPECT_EQ(tex_va.size, 2);
+        EXPECT_EQ(tex_va.usage, GL_TEXTURE_COORD_ARRAY);
+        EXPECT_EQ(tex_va.type, GL_FLOAT);
+        EXPECT_EQ(tex_va.stride, sizeof(test_struct_t));
+        EXPECT_EQ(tex_va.pointer, (const void*) offsetof(test_struct_t, uv));
+    }
+}
