ce8cf90f79af991446170626a92f5d80aa8b5f86
Swung0x48 <swung0x48@outlook.com>
2025-04-15 10:30:58 +0800

    [Tests]: remove junk

--- src/test/java/com/fcl/plugin/mobileglues/SanityCheckTest.kt
+++
@@ -1,9 +0,0 @@
-package com.fcl.plugin.mobileglues
-
-import androidx.test.ext.junitgtest.GtestRunner
-import androidx.test.ext.junitgtest.TargetLibrary
-import org.junit.runner.RunWith
-
-@RunWith(GtestRunner::class)
-@TargetLibrary(libraryName = "mobileglues_tests")
-class SanityCheckTest
