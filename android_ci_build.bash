b1dbb2b2354b84782e4fa6b74f564f6c8b2774fd
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-04-15 23:34:22 +0800

    Update android_ci_build.bash

--- android_ci_build.bash
+++ android_ci_build.bash
@@ -2,6 +2,7 @@
 # set -e
 
 git clone --depth 1 https://github.com/g-truc/glm.git src/main/cpp/include/glm
+git clone --depth 1 https://github.com/stbrumme/xxhash.git src/main/cpp/3rdparty/xxhash
 cmake_build () {
   ANDROID_ABI=$1
   mkdir -p build
